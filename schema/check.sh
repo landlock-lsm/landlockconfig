dc7b02e934fc8a0d904dd2beb4319fa02ddc8a3b
Mickaël Salaün <mic@digikod.net>
2025-02-16 13:29:14 +0100

    Improve JSON checks
    Check formatting for all JSON files including the schema.
    
    Improve check.sh with error details.
    
    Fix big integer parsing in JQ by upgrading to Ubuntu 24.04:
    
     diff -u ./schema/landlockconfig.json <(jq < ./schema/landlockconfig.json)
     --- ./schema/landlockconfig.json
     +++ /dev/fd/63
     @@ -11,7 +11,7 @@
          "uint64": {
            "type": "integer",
            "minimum": 0,
     -      "maximum": 18446744073709551615
     +      "maximum": 18446744073709552000
          },
          "file": {
            "anyOf": [
    
    See https://github.com/jqlang/jq/pull/1752
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- schema/check.sh
+++ schema/check.sh
@@ -4,4 +4,26 @@ set -u -e -o pipefail
 
 BASE_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-check-jsonschema --schemafile "${BASE_DIR}/landlockconfig.json" "$@"
+if [[ $# -eq 0 ]]; then
+    echo "ERROR: No files to check" >&2
+    exit 1
+fi
+
+for file in "$@"; do
+    if [[ ! -r "${file}" ]]; then
+        echo "ERROR: Invalid file: ${file}" >&2
+        exit 1
+    fi
+
+    diff -u "${file}" <(jq < "${file}") || {
+        echo
+        echo "ERROR: Invalid formatting" >&2
+        exit 1
+    }
+
+    check-jsonschema --schemafile "${BASE_DIR}/landlockconfig.json" "${file}" || {
+        echo
+        echo "ERROR: Not validated by schema: ${file}" >&2
+        exit 1
+    }
+done
